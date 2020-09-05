7f15aedc7a67147f9079d30235c6b1ccaa569b4c
mrpandey1 <pandeynishant12345@gmail.com>
2020-09-05 10:41:42 +0530

    initialized project

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
+
+import {BrowserRouter as Router ,Switch,Route} from 'react-router-dom';
+
+const Root=()=>(
+    <Router>
+        <Switch>
+            <Route path='/' component={App}>
+            </Route>
+        </Switch>
+    </Router>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'));
+registerServiceWorker();
