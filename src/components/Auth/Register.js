195868d2caad22eea8a17deae11069fd9866a8f3
mrpandey1 <pandeynishant12345@gmail.com>
2020-09-05 12:43:20 +0530

    changes in registration form

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -8,40 +8,71 @@ class Register extends React.Component{
         username:'',
         email:'',
         password:'', 
-        passwordConfirmation:''
+        passwordConfirmation:'',
+        errors:'',
+        loading:false
     }
     handleChange=event=>{
         this.setState({[event.target.name]:event.target.value})
-        };
+    }
+
     isFormValid=()=>{
+        let errors=[];
+        let error;
         if(this.isFormEmpty(this.state)){
-
-        }else if(!this.isPasswordValid()){
-
+            error=  {message:'Fill in all fields'};
+            this.setState({errors:errors.concat(error)})
+            return  false;
+        }else if(!this.isPasswordValid(this.state)){
+            error={message:'Password is invalid'}
+            this.setState({errors:errors.concat(error)});
+            return false;
         }else{
             return true;
         }
     }
-    isFormEmpty=({username,email,password,passwordConfirmation})=>{
-            return !username.length ||!email.length ||!password.length || !passwordConfirmation.length
+    isPasswordValid=({ password,passwordConfirmation})=>{
+        if(password.length<6 || passwordConfirmation.length<6){
+            return false ;
+        }
+        else if (password!==passwordConfirmation){
+            return false;
+        }else{
+            return true;
+        }
     }
 
-        handleSubmit=event=>{
-            if(this.isFormValid()){
-                event.preventDefault();
-                firebase.auth().createUserWithEmailAndPassword(this.state.email,this.state.password).then(
-                    createdUser=>{
-                        console.log(createdUser)
-                    }
-                ).catch(err=>{
-                    console.log(err)
-                });
-            }
+    isFormEmpty=({username,email,password,passwordConfirmation})=>{
+        return !username.length ||!email.length ||!password.length || !passwordConfirmation.length
+    }
 
+    displayErrors=errors=> errors.map((error,i)=><p key={i}>{error.message}</p>);
+
+    handleSubmit=event=>{
+        if(this.isFormValid()){
+            this.setState({errors:[],loading:true})
+            event.preventDefault();
+            firebase.auth().createUserWithEmailAndPassword(this.state.email,this.state.password).then(
+                createdUser=>{
+                    console.log(createdUser);
+                    createdUser.user.updateProfile({
+                        displayName:this.state.username,
+                        photoURL: `http://gravatar.com/avatar/${md5(createdUser.user.email)}?d=identicon`
+                    }).then(()=>{
+                        this.setState({loading:false});
+                    }).catch(err=>{
+                        console.log(err);
+                        this.setState({errors:this.state.errors.concat(err),loading:false})
+                    })
+                }
+            ).catch(err=>{
+                console.log(err)
+                this.setState({errors:this.state.errors.concat(err),loading:false});
+            });
         }
-
+    }
     render(){
-        const {username,password,email,passwordConfirmation}=this.state;
+        const {loading,username,password,email,passwordConfirmation,errors}=this.state;
         return(
             <Grid textAlign='center' verticalAlign='middle' className='app'>
                 <Grid.Column style={{maxWidth:450}}>
@@ -60,9 +91,15 @@ class Register extends React.Component{
                             <Form.Input fluid name='passwordConfirmation' icon='repeat' iconPosition='left'
                             value={passwordConfirmation}  placeholder='Password Confirmation' onChange={this.handleChange} type='password'/>
                         
-                            <Button color='orange' fluid size='large'>Submit</Button>
+                            <Button disabled={loading} className={loading?'loading':''} color='orange' fluid size='large'>Submit</Button>
                         </Segment>
                     </Form>
+                    {errors.length>0 && (
+                        <Message error>
+                            <h3>Error</h3>
+                            {this.displayErrors(errors)}
+                        </Message>
+                    )}
                     <Message>Already registered ?<Link to='/login'> Login </Link></Message>
                 </Grid.Column>
             </Grid>
