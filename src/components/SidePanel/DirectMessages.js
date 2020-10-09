22bef6868cf420c0ee362e4b98f2d8d4476e8ed1
mrpandey1 <pandeynishant12345@gmail.com>
2020-10-09 13:12:17 +0530

    created directMessages component

---
+++ src/components/SidePanel/DirectMessages.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Menu , Icon} from 'semantic-ui-react';
+class DirectMessages extends React.Component{
+    state={
+        users:[]
+    }
+    render(){
+        const {users}= this.state;
+        return (
+            <Menu.Menu className='menu'>
+                <Menu.Item>
+                    <span>
+                        <Icon name='mail'/>Direct Messages
+                    </span>{' '}
+                    ({ users.length })
+                </Menu.Item>
+                {/* Users to send direct messages */}
+            </Menu.Menu>
+        )
+    }
+}
+
+export default DirectMessages;
