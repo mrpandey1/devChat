880daee71f03fb47e3231e809792e4c0fb2d0691
mrpandey1 <pandeynishant12345@gmail.com>
2020-10-11 13:14:16 +0530

    display channel info in metapanel

--- src/components/MetaPanel/MetaPanel.js
+++ src/components/MetaPanel/MetaPanel.js
@@ -1,64 +1,71 @@
 import React from "react";
-import {Segment,Accordion,Header,Icon} from 'semantic-ui-react';
+import { Segment, Accordion, Header, Icon, Image } from "semantic-ui-react";
+
 class MetaPanel extends React.Component {
-  state={
-    privateChannel:this.props.isPrivateChannel,
-    activeIndex:0
-  }
-  setActiveIndex=(event,titleProps)=>{
-    const {index}=titleProps;
-    const {activeIndex}=this.state;
-    const newIndex=activeIndex===index?-1:index;
-    this.setState({activeIndex:newIndex});
-  }
+  state = {
+    channel: this.props.currentChannel,
+    privateChannel: this.props.isPrivateChannel,
+    activeIndex: 0
+  };
+
+  setActiveIndex = (event, titleProps) => {
+    const { index } = titleProps;
+    const { activeIndex } = this.state;
+    const newIndex = activeIndex === index ? -1 : index;
+    this.setState({ activeIndex: newIndex });
+  };
+
   render() {
-    const {activeIndex,privateChannel}=this.state;
-    if(privateChannel){
-      return null;
-    }
+    const { activeIndex, privateChannel, channel } = this.state;
+
+    if (privateChannel) return null;
+
     return (
-      <Segment>
-        <Header as='h3' attached='top'>
-          About # channel
+      <Segment loading={!channel}>
+        <Header as="h3" attached="top">
+          About # {channel && channel.name}
         </Header>
-        <Accordion styled attached='true'>
+        <Accordion styled attached="true">
           <Accordion.Title
-          active={activeIndex===0}
-          index={0}
-          onClick={this.setActiveIndex}
+            active={activeIndex === 0}
+            index={0}
+            onClick={this.setActiveIndex}
           >
-            <Icon name='dropdown'/>
-            <Icon name='info'/>
+            <Icon name="dropdown" />
+            <Icon name="info" />
             Channel Details
           </Accordion.Title>
-          <Accordion.Content activeIndex={activeIndex===0}>
-            Details
+          <Accordion.Content active={activeIndex === 0}>
+            {channel && channel.details}
           </Accordion.Content>
 
           <Accordion.Title
-          active={activeIndex===1}
-          index={1}
-          onClick={this.setActiveIndex}
+            active={activeIndex === 1}
+            index={1}
+            onClick={this.setActiveIndex}
           >
-            <Icon name='dropdown'/>
-            <Icon name='user circle'/>
+            <Icon name="dropdown" />
+            <Icon name="user circle" />
             Top Posters
           </Accordion.Title>
-          <Accordion.Content activeIndex={activeIndex===1}>
+          <Accordion.Content active={activeIndex === 1}>
             posters
           </Accordion.Content>
 
           <Accordion.Title
-          active={activeIndex===2}
-          index={2}
-          onClick={this.setActiveIndex}
+            active={activeIndex === 2}
+            index={2}
+            onClick={this.setActiveIndex}
           >
-            <Icon name='dropdown'/>
-            <Icon name='pencil alternate'/>
+            <Icon name="dropdown" />
+            <Icon name="pencil alternate" />
             Created By
           </Accordion.Title>
-          <Accordion.Content activeIndex={activeIndex===2}>
-            creator
+          <Accordion.Content active={activeIndex === 2}>
+            <Header as="h3">
+              <Image circular src={channel && channel.createdBy.avatar} />
+              {channel && channel.createdBy.name}
+            </Header>
           </Accordion.Content>
         </Accordion>
       </Segment>
