391205f5fb01f2421a3fefa5d950e8324521a4b0
mrpandey1 <pandeynishant12345@gmail.com>
2020-10-11 20:22:17 +0530

    display saved user colors

--- src/components/App.js
+++ src/components/App.js
@@ -8,13 +8,18 @@ import SidePanel from "./SidePanel/SidePanel";
 import Messages from "./Messages/Messages";
 import MetaPanel from "./MetaPanel/MetaPanel";
 
-const App = ({ currentUser, currentChannel ,isPrivateChannel,userPosts}) => (
-  <Grid columns="equal" className="app" style={{ background: "#eee" }}>
+// prettier-ignore
+const App = ({ currentUser, currentChannel, isPrivateChannel, userPosts, primaryColor, secondaryColor }) => (
+  <Grid columns="equal" className="app" style={{ background: secondaryColor }}>
     <ColorPanel
-    key={currentUser && currentUser.name}
+      key={currentUser && currentUser.name}
       currentUser={currentUser}
     />
-    <SidePanel key={currentUser && currentUser.uid} currentUser={currentUser} />
+    <SidePanel
+      key={currentUser && currentUser.uid}
+      currentUser={currentUser}
+      primaryColor={primaryColor}
+    />
 
     <Grid.Column style={{ marginLeft: 320 }}>
       <Messages
@@ -27,10 +32,11 @@ const App = ({ currentUser, currentChannel ,isPrivateChannel,userPosts}) => (
 
     <Grid.Column width={4}>
       <MetaPanel
-      key={currentChannel && currentChannel.name} 
-      userPosts={userPosts}
-      currentChannel={currentChannel}
-      isPrivateChannel={isPrivateChannel} />
+        key={currentChannel && currentChannel.name}
+        userPosts={userPosts}
+        currentChannel={currentChannel}
+        isPrivateChannel={isPrivateChannel}
+      />
     </Grid.Column>
   </Grid>
 );
@@ -38,8 +44,10 @@ const App = ({ currentUser, currentChannel ,isPrivateChannel,userPosts}) => (
 const mapStateToProps = state => ({
   currentUser: state.user.currentUser,
   currentChannel: state.channel.currentChannel,
-  isPrivateChannel:state.channel.isPrivateChannel,
-  userPosts:state.channel.userPosts
+  isPrivateChannel: state.channel.isPrivateChannel,
+  userPosts: state.channel.userPosts,
+  primaryColor: state.colors.primaryColor,
+  secondaryColor: state.colors.secondaryColor
 });
 
 export default connect(mapStateToProps)(App);
